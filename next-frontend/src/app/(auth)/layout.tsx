5f0d37b61e5b319c215fda18427aa3d16d993f7e
Hareem <hareem@ccript.com>
2025-05-31 04:10:07 +0500

    chore: removed nested html tags

--- next-frontend/src/app/(auth)/layout.tsx
+++ next-frontend/src/app/(auth)/layout.tsx
@@ -1,19 +1,6 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "../../app/globals.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "@fortawesome/fontawesome-free/css/all.min.css";
 import ClientLayout from "@/shared/client-layout";
+import type { Metadata } from "next";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -26,12 +13,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <ClientLayout>{children}</ClientLayout>
-      </body>
-    </html>
+
+    <ClientLayout>{children}</ClientLayout>
+
   );
 }
